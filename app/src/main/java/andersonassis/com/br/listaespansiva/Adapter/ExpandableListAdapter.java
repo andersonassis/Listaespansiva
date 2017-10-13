71241255608cb881b9f6da2cf3f097e6760204e7
andersonassis <anderson1460@hotmail.com>
2017-10-13 11:32:54 -0300

    commit2

---
+++ app/src/main/java/andersonassis/com/br/listaespansiva/Adapter/ExpandableListAdapter.java
@@ -0,0 +1,109 @@
+package andersonassis.com.br.listaespansiva.Adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import andersonassis.com.br.listaespansiva.R;
+
+/**
+ * Created by aassis on 11/10/2017.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context _context;
+    private List<String> _listDataHeader; // header titles
+    private HashMap<String, List<String>> _listDataChild;
+
+
+    public ExpandableListAdapter(Context context, List<String> listDataHeader, HashMap<String, List<String>> listChildData) {
+
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+
+
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return this._listDataChild.get(this._listDataHeader.get(i))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return this._listDataHeader.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return this._listDataChild.get(this._listDataHeader.get(i))
+                .get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_group, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition, childPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_item, null);
+        }
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListItem);
+
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
